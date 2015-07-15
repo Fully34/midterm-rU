47d5485a1225e79a0538b7ab1f57a129cca9211c
Fully34 <cfullinw34@gmail.com>
2015-07-15 10:56:10 -0600

    done with landing page for now, moving on to shop page

--- main.js
+++ main.js
@@ -10,58 +10,41 @@ $(function() {
 
     //============================== hover text on sub-img ==============================//
     
-    // Create fresh elements to frankenstein
-    var hoverText = function() {
+    $('.img-container').mouseenter(function(){
 
-        var els = {
+        var theHover = $(this).find('.hover-container');
 
-            cont    : $("<div class='container'></div>"),
-            title   : $("<h1 class='hover-title'></h1>"),
-            par     : $("<p class='hover-description'></p>")
+        if ( $(this).hasClass('first-sub') || $(this).hasClass('second-sub') ) {
+
+            theHover.removeClass('hidden').animate({marginTop:'15px'},'slow');
+
+        } else if ( $(this).hasClass('third-sub') ) {
+
+           if ($(window).width() > 550){
+
+                theHover.removeClass('hidden').animate({marginTop:'5px'},'slow');
+                
+                theHover.find('.btn').removeClass('hidden');
+
+            } else {
+
+                $(this).find('.btn').removeClass('hidden').animate({marginTop:'30px'},'slow');
+            }
         }
-
-        return els;
-    }
-
-    var frankenstein = function(title, description) {
-
-        var els = hoverText();
-
-        var container = els.cont;
-        var titleText = els.title;
-        var para = els.par;
-
-        //set title text
-        titleText.text(title);
-
-        //set description text
-        para.text(description);
-
-
-        container.append(titleText, para);
-
-        return container; 
-    }
-
-    // hover effects.
-    $('.first-sub').on('mousemove', function() {
-        
-        
-
-        $(this).css({
-
-            'opacity': '0.3'
-        })
-
-        $(this).append()
     });
 
-    $('.sub-img').on('mouseout', function() {
+    $('.img-container').mouseleave(function(){
 
-        $(this).css({
+        $(this).find('.hover-container').addClass('hidden').css({
 
-            'opacity': '1'
+        'margin-top': '-50px'
+        })
+
+        $(this).find('.shop-btn').addClass('hidden').css({
+
+            'margin-top': '-20px'
         })
     });
-    
+
+
 });
