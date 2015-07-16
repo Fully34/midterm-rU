4f38252a223106fb8aba7217ea18554329155bdf
Fully34 <cfullinw34@gmail.com>
2015-07-16 11:53:50 -0600

    working on queue

--- main.js
+++ main.js
@@ -57,29 +57,44 @@
 
     //============================== global variables ==============================//
 
-    var progressArr = ['Engage!', 'Sequence Initiate!', 'And... Here... We... Go...', 'Makin\'n Moves!', 'Let\'s Do This!' ]   
+    var quoteArr = ['Engage!', 'Sequence Initiate!', 'Annnd here... We... Go...', 'Makin\'n Moves!', 'Let\'s do this!', '#BOOM', 'No Sweat!', 'Break the wrist. Walk away.', 'JACKPOT!', 'One Small Step For Man!', 'SWEEP THE LEG!', 'Nobody makes me bleed my own blood!', 'We\'re gonna need a bigger boat', 'Heeeeeere\'s Johnny!', 'It\'s party time!', 'ARRGGGHHH,  KELLY CLARKSON!', 'There\'s no crying in baseball!', 'I\'ll have what she\'s having', 'The price is wrong']   
 
-    var completeArr = ['#BOOM', 'No Sweat!', 'I Will Take All Challengers!'];
+    //HANDLEBARS TEMPLATE COMPILE -- FOR INCOMPLETE TABLE
+    var templateScript = $('#table-data-script').html();
+
+    var tableDataTemplate = Handlebars.compile(templateScript);
 
     var incompleteCustomerData = {customers: [] };
 
-    var completeCustomerData = {customers: []};
+    //HANDLEBARS TEMPLATE COMPILE -- FOR RECENTLY SHIPPED TABLE
+    var templateScriptComplete = $('#complete-table-script').html();
+
+    var completeTableTemplate = Handlebars.compile(templateScriptComplete);
+
+    var completeCustomerData = {customers: [], customersToRender : [] };
 
     // global var for order numbers
     var orderCount = 1;
 
     //========================= customer object constructor=========================//
 
+
     var Customer = function(order, name, address, type) {
 
         this.order = order;    
         this.name = name;
         this.address = address || null;
-        this.paid = null;
-        this.inProgress = null;
-        this.complete = null;
-        this.shipped = null; 
+        //===============//
+        // NEED TO ALTER THIS WHEN WE LEARN BACK-END STUFF!!!! IE: when somebodies payment goes thru, this gets set to true
+        this.paid = true;
+        // ==============//
+        this.inProgress = false;
+        this.complete = false;
+        this.shipped = false; 
         this.type = type || null;
+        this.progressRando = _.sample(quoteArr, 1);
+        this.completeRando = _.sample(quoteArr, 1);
+        this.shippedRando = _.sample(quoteArr, 1);
     }
 
     Customer.prototype.isPaid = function() {
@@ -100,11 +115,19 @@
     Customer.prototype.isShipped = function() {
 
         this.shipped = true;
+        this.dateShipped = new Date();
     }
+
+    // dummy customer 
+    // var cust1 = new Customer(orderCount, 'Chris Fullinwider', '123 Awesome St.', 'Rack');
+    // var theArray = incompleteCustomerData.customers;
+
+    // theArray.push(cust1);
 
     //============================== submit button ==============================//
 
-    //only checking if there is actually something there in each input field
+    //only checking if there is actually something there in each input field 
+        //> more sophisticated validation will be used in the final site
     var validate = function(string) {
 
         if ( (string.length <= 0) ) {
@@ -133,31 +156,38 @@
         // FORM VALIDATION
         if (!valName) {
 
-            $(this).siblings('#name-input').addClass('invalid-form');
+            var errorInName = $(this).siblings('#name-input');
+            errorInName.addClass('invalid-form');
+            errorInName.val("NEED TEXT!!!!");
 
             setTimeout(function(){
 
-                $('#name-input').removeClass('invalid-form');
+                $('#name-input').removeClass('invalid-form').val('');
             }, 1000);
         }
 
         if (!valAddress) {
 
-            $(this).siblings('#address-input').addClass('invalid-form');
+            var errorInAddress = $(this).siblings('#address-input');
+            errorInAddress.addClass('invalid-form');
+            errorInAddress.val("NEED TEXT!!!!");
 
             setTimeout(function(){
 
-                $('#address-input').removeClass('invalid-form');
+                $('#address-input').removeClass('invalid-form').val('');
             }, 1000);
         }
 
         if ( (!valType) ) {
 
-            $(this).siblings('#type-input').addClass('invalid-form');
+            var errorInType = $(this).siblings('#type-input');
+
+            errorInType.addClass('invalid-form');
+            errorInType.val("NEED TEXT!!!");
 
             setTimeout(function(){
 
-                $('#type-input').removeClass('invalid-form');
+                $('#type-input').removeClass('invalid-form').val('');
             }, 1000);
         }
 
@@ -168,6 +198,7 @@
 
             if ( ensure ) {
 
+                // nuke and repave    
                 $('.queue-table-body').html('');
 
                 var newCust = new Customer(orderCount, name, address, type); 
@@ -183,11 +214,8 @@
                 $(this).siblings('#address-input').val('');
                 $(this).siblings('#type-input').val('');
 
-                var templateScript = $('#table-data-script').html();
-
-                var tableDataTemplate = Handlebars.compile(templateScript);
-
-                $('.queue-table-body').append(tableDataTemplate(incompleteCustomerData))
+                // use handlebars to generate the table on the fly
+                $('.queue-table-body').append(tableDataTemplate(incompleteCustomerData));
 
             } else {
 
@@ -196,12 +224,100 @@
         }
     });
     
-    //============================== FIRST ATTEMPT AT HANDLEBARS ==============================//
+    //======================= replacing the button on the queue ==============================//
+    
+    $('.table').on('click', '.table-check', function(){
+
+        // DOM traversal to link action to data inside incompleteCustomerData.customers array 
+        var parentRow = $(this).closest('.table-row');
+        // need to return data-order attribute as an integer, it returns a string by default
+        var orderNumber = parseInt( parentRow.find('.orderData').attr('data-order'), 10);
+        var theCustomer = returnTheCustomer(orderNumber);
+
+        if ( theCustomer.paid ) {
+
+            var yesNo = confirm("Are you really done, Cameron?");
+
+            if ( yesNo ) {
+
+                // var theP = $(this).siblings('.chain-item');
+
+                // $(this).addClass('hidden');
+                // theP.removeClass('hidden');
+
+                // //pick text at random from the fun quotes array
+                // var quote = _.sample(progressArr, 1); 
+                // theP.text(quote);
+
+                // check which button we pressed and then modify the customer's data
+                if ( $(this).hasClass('progress-check') ) {
+
+                    $('.queue-table-body').html('');
+                    theCustomer.isInProgress();
+                    $('.queue-table-body').append(tableDataTemplate(incompleteCustomerData));
+
+                } else if ( $(this).hasClass('complete-check') ) {
+
+                    $('.queue-table-body').html('');
+                    theCustomer.isComplete();
+                    $('.queue-table-body').append(tableDataTemplate(incompleteCustomerData));
+
+                } else if ( $(this).hasClass('shipped-check') ) {
+                    
+                    var incompleteArr = incompleteCustomerData.customers;
+                    var completeArr = completeCustomerData.customers;
+                    var completeArrRender = completeCustomerData.customersToRender;
+                    completeArrRender = [];
+
+                    completeArr.push(theCustomer);
+                    theCustomer.isShipped();
+
+
+                    // take this customer out of the incomplete array and push it to the complete array
+                        // then push it to template and the completed table
+                    for (var i = 0; i < incompleteArr.length; i++) {
+
+                        if (incompleteArr[i].date === theCustomer.date) {
+
+                            incompleteArr.splice(i, 1);
+                        }  
+                    };
+
+                    $('.queue-table-body').html('');
+                    $('.queue-table-body').append(tableDataTemplate(incompleteCustomerData));
+
+                    for (var i = completeArr.length; i > completeArr.length-5; i--) {
+
+                        completeArrRender.push(completeArr[i]); 
+                    };
+
+                    $('.shipped-table-body').append(completeTableTemplate(completeCustomerData))
+                    // $('.shipped-table-body').append(completeTableTemplate(completeCustomerData))
+
+                }
+
+            } else {
+
+                alert("I knew you were lying");
+            }
+
+        } else {
+
+            alert("Don't make them anything!  They haven't PAID!")
+        }
+    });
 
 
 
+    var returnTheCustomer = function(orderNum) {
 
+        var arr = incompleteCustomerData.customers;
 
+        // find the customer and set to variable
+        var theCustomer = _.find(arr, function(obj) { return obj.order === orderNum; });
+
+        return theCustomer;
+    };
 
 
 
